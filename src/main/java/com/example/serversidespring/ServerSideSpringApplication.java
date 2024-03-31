9629a86b9fe143efcae6c3b23d9649966b64ef66
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-03-31 22:48:04 +0200

    Init

---
+++ src/main/java/com/example/serversidespring/ServerSideSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversidespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSideSpringApplication.class, args);
+    }
+
+}
