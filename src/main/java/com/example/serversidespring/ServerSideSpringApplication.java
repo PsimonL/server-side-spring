02411eb26ad0b01c648c294e41501051ebdf51fe
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-04-02 00:57:52 +0200

    WebSocket config

--- src/main/java/com/example/serversidespring/ServerSideSpringApplication.java
+++ src/main/java/com/example/serversidespring/ServerSideSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerSideSpringApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting Spring Chat Parring Server...");
         SpringApplication.run(ServerSideSpringApplication.class, args);
     }
 
