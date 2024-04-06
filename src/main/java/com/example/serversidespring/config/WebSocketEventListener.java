46a4dafccbbf887a63b06b39bbb86e45f0891fab
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-04-06 18:01:21 +0200

    Refactor

---
+++ src/main/java/com/example/serversidespring/config/WebSocketEventListener.java
@@ -0,0 +1,4 @@
+package com.example.serversidespring.config;
+
+public class WebSocketEventListener {
+}
--- src/main/java/com/example/serversidespring/configuration/WebSocketConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.serversidespring.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig {
-}
