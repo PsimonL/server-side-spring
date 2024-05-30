cc02852fd1b5bc7440ac9c418f1f1132a286bef4
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-30 14:46:44 +0200

    Bunch of changes: pure server config, without client side, adding CorsConfig.java (Cross-Origin Resource Sharing)

--- src/main/java/com/example/serversidespring/config/WebSocketConfig.java
+++ src/main/java/com/example/serversidespring/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();  // WebSocket Endpoint
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");  // Incoming messages
+        registry.enableSimpleBroker("/topic");  // Outgoing messages
     }
 }
--- src/main/java/com/example/serversidespring/controller/ChatController.java
+++ src/main/java/com/example/serversidespring/controller/ChatController.java
@@ -9,20 +9,20 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")
+    @MessageMapping("/message")
     @SendTo("/topic/public")
-    public ChatMessage sendMessage(
-            @Payload ChatMessage chatMessage
-    ) {
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/private-message")
+    public ChatMessage sendPrivateMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(
-            @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
-    ) {
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
