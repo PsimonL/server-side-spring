582691120a63d54b364a2ecaaad453814c9ecc5e
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-06-03 18:01:31 +0200

    Resetting, restart ideas

--- src/main/java/com/example/serversidespring/repository/UserRepository.java
+++ src/main/java/com/example/serversidespring/repository/UserRepository.java
@@ -1,10 +1,8 @@
-package com.example.serversidespring.repository;
-
-import com.example.serversidespring.entity.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<AppUser, Long> {
-    Optional<AppUser> findByUsername(String username);
-}
+//package com.example.serversidespring.repository;
+//
+//import com.example.serversidespring.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByFirstNameAndLastName(String firstName, String lastName);
+//}
